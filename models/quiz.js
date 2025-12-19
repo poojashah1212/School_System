958790581394aa8fb47ce0c67a3afaaf77a40e46
Pooja Shah <poojashah12108@gmail.com>
2025-12-19 14:05:15 +0530

    quiz schema

---
+++ models/quiz.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema(
+  {
+    question: { type: String, required: true, trim: true },
+    optionA: { type: String, required: true },
+    optionB: { type: String, required: true },
+    optionC: { type: String, required: true },
+    optionD: { type: String, required: true },
+    correctOption: {
+      type: String,
+      enum: ["A", "B", "C", "D"],
+      required: true
+    },
+    marks: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    teacherId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Quiz", quizSchema);
