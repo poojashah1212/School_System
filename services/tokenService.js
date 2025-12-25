d7013008e15343577f67e5ea0bf854e9d42616d8
Pooja Shah <poojashah12108@gmail.com>
2025-12-25 09:29:43 +0530

    token

--- services/tokenService.js
+++ services/tokenService.js
@@ -4,7 +4,7 @@ const createToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_SECRET || 'dev_secret',
-    { expiresIn: process.env.JWT_EXPIRES_IN || '10d' }
+    { expiresIn: process.env.JWT_EXPIRES_IN || '50d' }
   );
 };
 
